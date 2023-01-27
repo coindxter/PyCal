acb5651baa80451e684d8b00975ceb826c0bfeb5
Owen Crace <owencrace@gmail.com>
2023-01-27 10:59:54 -0600

    added src

---
+++ src/typing.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import time
+
+def typingPrint(text):
+  for character in text:
+    sys.stdout.write(character)
+    sys.stdout.flush()
+    time.sleep(0.05)
+  
+def typingInput(text):
+  for character in text:
+    sys.stdout.write(character)
+    sys.stdout.flush()
+    time.sleep(0.05)
+  value = input()  
+  return value
+
+def clearScreen():
+  os.system("cls")
